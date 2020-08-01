8915811acbd7ce05776f111a9581a1e810e1f44d
Jorge Luis Aguilar <31743985+jorch17@users.noreply.github.com>
2020-07-31 22:02:39 -0500

    base de datos remota
    Modifique los parametros de conexion para la base de datos remota

--- Tulanmap/config.py
+++ Tulanmap/config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = 'localhost'
-db_name = 'kuorra_login'
-db_user = 'kuorra'
-db_pw = 'kuorra.2018'
+db_host = 'u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'bix2j6yp4ioah07i'
+db_user = 'glf2q4q93bhipqmm'
+db_pw = 'y8i4y8lqboy4y4u3'
 
 db = web.database(
     dbn='mysql',
